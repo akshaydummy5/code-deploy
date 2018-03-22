652ac5951bb2bfb7111313e0c035b07594036dca
akshaydummy5 <37642784+akshaydummy5@users.noreply.github.com>
2018-03-22 10:12:50 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Deployed Properly' });
+  res.render('index', { title: 'Git auto deploy feature' });
 });
 
 module.exports = router;
